a2109b0854691519dbfb4297360d37aafc667332
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-25 17:56:12 +0800

    [fufangyuan] add route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, NavLink,Switch,Route} from 'react-router-dom';
 import Home from './components/Home';
 import Order from './components/Order';
+import AddProduct from './components/AddProduct';
 
 
 class App extends Component {
@@ -13,11 +14,13 @@ class App extends Component {
         <nav className = 'bar'>
           <NavLink to='/' activeStyle={{backgroundColor:'blue'}}>商城</NavLink>
           <NavLink to='/order' activeStyle={{backgroundColor:'blue'}}>订单</NavLink>
+          <NavLink to='/addProduct' activeStyle={{backgroundColor:'blue'}}>+添加商品</NavLink>
         </nav>
       </header>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/order" component={Order} />
+        <Route exact path="/addProduct" component={AddProduct} />
       </Switch>
       </BrowserRouter>
     )
